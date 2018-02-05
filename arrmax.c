753a077dd3461f725237785b21a92d2f0524eb0a
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-02-05 16:03:54 +0530

    arrmax.c

---
+++ arrmax.c
@@ -0,0 +1,22 @@
+#include<stdio.h.
+int main()
+{
+int array[50],maximum,i,size,location=0;
+printf("enter the number of element in array\n");
+scanf('%d",&size);
+printf("enter the %d number",size);
+{
+for(i=0;i<size;i++)
+scanf("%d",array[i]);
+maximum=array[0];
+for(i=1;i<size;i++)
+{
+if(array[i]>maximum)
+{
+maximum=array[i];
+location=i+1;
+}
+}
+printf("maximum value location %d and its value %d",location,maximum);
+return 0;
+}
