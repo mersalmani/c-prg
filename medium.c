ce364a7d4affe57e3de24f7d40e70060d1ad26a0
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-02-05 16:48:10 +0530

    medium.c

---
+++ medium.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int array[100],i,j,size,medium;
+printf("enter the array elements are");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+if(arr[i]==arr[j]);
+printf("the elements are",size);
+array[i]<=medium[i];
+printf("the medium element is %d":);
+return 0;
+
+}
+
