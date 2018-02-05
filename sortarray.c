c19a537ec3cafb0e54970a0957534b6bf68d5d4b
mersalmani <30549798+mersalmani@users.noreply.github.com>
2018-02-05 16:30:09 +0530

    sortarray.c

---
+++ sortarray.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int array[100];
+int size;
+int i,j,temp;
+printf("enter the array of element is:");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+if(aray[i]>arr[j]);
+{
+temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+for(j=0;j<size;j++)
+{
+printf("the valuse are:",size);
+printf("the sorted values are %d\n is");
+}
+return 0;
+}
